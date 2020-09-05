3b0f5650c8570108024a26ff98ce7a9b7bf037f0
Sachin Bhola <sbsachin699@gmail.com>
2020-09-05 11:53:45 +0530

    added authGuard service , dashboard module and login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
